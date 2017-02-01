d9ee9c48fa549ab3c3aa38559ec900826f744fb9
gold <ccunningham@sjnma.org>
2017-02-01 17:29:00 -0600

    Problem3.py complete

---
+++ Problem3.py
@@ -0,0 +1,14 @@
+number = int(raw_input("Hello, number please" '>'))
+
+mood = raw_input("Please insert mood" '>')
+
+a =int(5)
+b =int(25)
+
+if( a <= number ):
+    if( b >= number ):
+	print("In range")
+if( mood == "Good" or mood == "good"
+    and number < a
+        or number > b):
+            print(":-0")
