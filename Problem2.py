16714e6cac992bc8f59f480244c8ac7f492fc448
gold <ccunningham@sjnma.org>
2017-02-01 17:07:35 -0600

    Problem2.py complete

---
+++ Problem2.py
@@ -0,0 +1,12 @@
+firstnumber = float(raw_input("first number"))
+secondnumber = float(raw_input("second number"))
+thirdnumber = float(raw_input("third number"))
+
+if(firstnumber < secondnumber < thirdnumber):
+	print("Up and rising")
+if(firstnumber > secondnumber > thirdnumber):
+	print("Falling down")
+if(firstnumber < secondnumber > thirdnumber)
+	print("Over the mountain")
+if(firstnumber > secondnumber < thirdnumber)
+	print("In the dog house")
