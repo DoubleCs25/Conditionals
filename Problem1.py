27a81fe0068baee9e04625894e1ac6cf2766762b
gold <ccunningham@sjnma.org>
2017-02-01 16:42:00 -0600

    Problem1.py complete

---
+++ Problem1.py
@@ -0,0 +1,16 @@
+a = int(raw_input("Please give me a number"))
+
+b = int(raw_input("Please give me another number"))
+
+c = int(raw_input("Please give me another number"))
+
+
+if( a + b == c):
+    print("true")
+
+elif( b + c == a):
+    print("true")
+
+elif( c + a == b):
+    print("true")
+
